24389b69df46f8abb6bfdad4ee819451e3c551a6
zeliha pala <zelihapala78@gmail.com>
2023-12-06 21:50:14 +0000

    sample test

--- time-string/time-string.test.js
+++ time-string/time-string.test.js
@@ -1,1 +1,9 @@
-// time-string.test.js
+function getOrdinalNumber() {
+  return "1st";
+}
+
+test("works for any number ending in 1", function () {
+  expect(getOrdinalNumber(1)).toBe("1st");
+  expect(getOrdinalNumber(11)).toBe("11th");
+  expect(getOrdinalNumber(21)).toBe("21st");
+});
