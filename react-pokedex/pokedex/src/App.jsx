ed27242caaa642e7d55578950eeae3ecabfb940c
Zeliha Pala <zelihapala78@gmail.com>
2024-02-28 23:46:17 +0000

    Composing components, rendering lists with keys

--- react-pokedex/pokedex/src/App.jsx
+++ react-pokedex/pokedex/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function CaughtPokemon() {
+  const date = new Date().toLocaleDateString();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <p>Caught 0 Pokemon on {date}</p>;
 }
 
-export default App
+function Logo() {
+  const appName = "Zeliha's Pokedex";
+  return (
+    <div>
+      <header>
+        <h1>Welcome to {appName}</h1>
+        <img
+          src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/040.png"
+          alt="Pokedex Logo"
+        />
+      </header>
+    </div>
+  );
+}
+
+function BestPokemon() {
+  const abilities = ["Anticipation", "Adaptability", "Run-Away"];
+
+  return (
+    // <div>
+    //   <p>My favorite Pokemon is Squirtle</p>
+    //   <p>Abilities: {abilities.join(', ')}</p>
+    // </div>
+    <div>
+      <p>My favorite Pokemon is Squirtle</p>
+      <ul>
+        Abilities:
+        {abilities.map((ability, index) => (
+          <li key={index}>{ability}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Logo />
+      <BestPokemon />
+      <CaughtPokemon />
+    </div>
+  );
+}
+
+export default App;
