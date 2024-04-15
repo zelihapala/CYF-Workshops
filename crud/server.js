70623eb2787f1bfcf294d4a4a4df4b6e1091cd69
Zeliha Pala <zelihapala78@gmail.com>
2024-04-15 01:25:45 +0100

    get done

--- crud/server.js
+++ crud/server.js
@@ -2,6 +2,17 @@ const express = require("express");
 const albumsData = require("./albums.json");
 const app = express();
 
+app.get("/albums", (req, res) => {
+  res.send(albumsData);
+});
+
+app.get("/albums/:albumId", (req, res) => {
+  const albumId = req.params.albumId;
+
+  const album = albumsData.find((album) => album.albumId === albumId);
+  res.json(album);
+});
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
